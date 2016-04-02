c89f5c914e7204ed037627a6b0945c19842df35c
Elom Gomez <gomezelom@yahoo.com>
2016-04-02 15:53:17 -0500

    Setup rubymotion specific project initializer in lib/enchanted_quill

--- lib/enchanted_quill.rb
+++ lib/enchanted_quill.rb
@@ -1,5 +1,11 @@
 require "enchanted_quill/version"
 
-module EnchantedQuill
-  # Your code goes here...
+unless defined?(Motion::Project::App)
+  raise "This must be required from within a RubyMotion Rakefile"
+end
+
+Motion::Project::App.setup do |app|
+  lib_dir = File.join(File.dirname(__FILE__))
+  app.files.unshift(Dir.glob(File.join(lib_dir, "enchanted_quill/**/*.rb")))
+  app.frameworks += %{ Foundation UIKit }
 end
